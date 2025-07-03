018b45d323f33ac1233b97f9bc29351229f2a89e
Devansh <122096076+dixitdevansh22@users.noreply.github.com>
2025-07-03 09:09:59 +0530

    Add files via upload

---
+++ GreedyApproach/mincoins.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class mincoins {
+    public static void main(String[] args) {
+        int amount = 93;
+        int[] coins ={1,2,5,10,20,50,100};
+        Arrays.sort(coins);
+        int count =0;
+        for (int i = coins.length-1; i>=0; i--) {
+            while(amount >=coins[i]){
+                amount -=coins[i];
+                count++;
+            }
+            
+        }
+        System.out.println(count);
+
+    }
+}
